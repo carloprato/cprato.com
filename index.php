7e7aacf3582f5c07498426f2a7c972ec170501e7
Carlo Prato <carlo@cprato.com>
2015-05-05 23:36:39 +0200

    Corrected parameters passed through URL

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <?php
-  #remove the directory path we don't want
-  $request  = str_replace("/envato/pretty/php/", "", $_SERVER['REQUEST_URI']);
- 
-  #split the path by '/'
-  $params     = split("/", $request);
-	$_GET['lang'] = $params['1'];
-	$_GET['p'] = $params['2'];
-  ?>
-
-<?php
-	if ($_GET['lang'] == NULL) $_GET['lang'] = 'en';
+	
+  	$params     	= split("/", $_SERVER['REQUEST_URI']);
+	$_GET['lang'] 	= $params['1'];
+	$_GET['p'] 		= $params['2'];
+ 	if ($_GET['lang'] == NULL) $_GET['lang'] = 'en';
 	if ($_GET['p'] == NULL) $_GET['p'] = 'home';
 	include("lang/en.lang.php");	
 	include("lang/" . $_GET['lang'] .".lang.php");
