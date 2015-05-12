c8adccaeb0793625e43b7365158311dc8566726c
Carlo Prato <carlo@cprato.com>
2015-05-13 00:30:21 +0200

    Early commit for authentication module.

---
+++ inc/classes/auth.class.php
@@ -0,0 +1,35 @@
+<?php
+	
+	class Auth {
+		
+		function login() {
+								
+		      $list = [];
+		      $db = Db::getInstance();
+		      $req = $db->query('SELECT * FROM users');
+		
+		      foreach($req->fetchAll() as $post) {
+		        $list[] = new Posts($post['id'], $post['user'], $post['password']);
+		      }
+		
+		      return $list;
+		    }
+	
+		}
+		
+		function logout() {
+			
+			
+		}
+		
+		function register() {
+			
+			
+		}
+		
+		function protect() {
+			
+			
+		}
+		
+	}
