0f3cc82423cb4c5b724ce539dcea78d9a246ca87
Carlo Prato <carlo@cprato.com>
2015-12-02 15:18:46 +0100

    Fixed core class

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 	require_once 'inc/classes/autoloader.class.php'; 	// Autoload classes
 	require_once 'data/config/db.config.php'; 			// Database details
     require_once 'vendor/autoload.php'; 				// Composer	
-	$routes	  = new Routes; 							// Router	
 	$core     = new Core; 								// Class with basic functions
+	$routes	  = new Routes; 							// Router	
 	Language::$lang = Language::load(LANG); 			// Loads language	
 	$tpl 	  = new TemplateController;					// Starts template
 	echo $tpl -> view(); 								// Let's go!
